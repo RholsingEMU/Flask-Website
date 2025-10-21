d112234d48e8483e7935997a5cfa63104ae871ed
RholsingEMU <ravi.holsinger@emu.edu>
2025-10-21 13:22:53 -0400

    Added iterable data and post request that goes nowhere.

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from random import randint
+
+class Profile:
+    name="Default"
+    desc="Default"
+    likes=0
+    #Constructor always looks like this
+    def __init__(self):
+        pass
+
+
+p = [ Profile(), Profile(), Profile() ] #Make a list with an object in it
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-    #return f"<p>WAGA my BAGA please. The number {str(randint(0, 20))}</p><a href=localhost:5000/waga>waga</a>"
+    return render_template("index.html", profiles=p)
 
 @app.route("/create-post")
 def waga():
@@ -15,3 +25,11 @@ def waga():
 @app.route("/profile")
 def bobo():
     return render_template("profile.html")
+
+@app.route("/submit", methods=["POST"])
+def postSubmit():
+    postName = request.form["postName"]
+    postDesc = request.form["postDesc"]
+    print(postName + " " + postDesc)
+    return "Post Post Posted"
+
