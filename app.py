6887d032005103436e9c16d4f60135f7d31fbc06
RholsingEMU <ravi.holsinger@emu.edu>
2025-10-28 14:21:56 -0400

    Added backend for the database

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, request, redirect
-from random import randint
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sqlalchemy.orm as so
+import sqlalchemy as sa
 
-class Profile:
-    name="Default"
-    desc="Default"
-    likes=0
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.db") #app.db is database name
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Profile(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name:so.Mapped[str] = so.mapped_column(index=True, default="Default name") #Type hinting
+    desc:so.Mapped[str] = so.mapped_column(index=True, default="Default desc")
+    likes:so.Mapped[int] = so.mapped_column(default=0)
     #Constructor always looks like this
     def __init__(self):
         pass
 
-p = [ Profile(), Profile(), Profile() ] #Make a list with an object in it
+#flask db init then flask db upgrade
 
-app = Flask(__name__)
+p = [ Profile(), Profile(), Profile() ] #Make a list with an object in it
 
 @app.route("/")
 def home():
