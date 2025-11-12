d939ddf10a5a67e99cb84f64ad7c1c82ce1c0853
RholsingEMU <ravi.holsinger@emu.edu>
2025-11-11 23:35:42 -0500

    Added login page and backend

--- app.py
+++ app.py
@@ -1,31 +1,54 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from flask_login import current_user, login_user
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sqlalchemy.orm as so
 import sqlalchemy as sa
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.db") #app.db is database name
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 class Profile(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name:so.Mapped[str] = so.mapped_column(index=True, default="Default name") #Type hinting
+    id: so.Mapped[int] = so.mapped_column(primary_key=True) #a:int = 5 <- type hinting
+    name:so.Mapped[str] = so.mapped_column(index=True, default="Default name")
     desc:so.Mapped[str] = so.mapped_column(index=True, default="Default desc")
     likes:so.Mapped[int] = so.mapped_column(default=0)
+
     #Constructor always looks like this
     def __init__(self):
         pass
 
+class User(UserMixin, db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(unique=True, index=True)
+    password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 #flask db init then flask db upgrade
 
-p = [ Profile(), Profile(), Profile() ] #Make a list with an object in it
+#p = [ Profile(), Profile(), Profile() ] #Make a list with an object in it
 
 @app.route("/")
 def home():
+    query = sa.select(Profile)
+    p = db.session.scalars(query).all()
+
+    #db.session.commit()
+
     return render_template("index.html", profiles=p)
 
 @app.route("/create-post")
@@ -40,6 +63,50 @@ def profile():
 def postSubmit():
     postName = request.form["postName"]
     postDesc = request.form["postDesc"]
+
+    obj = Profile(name=postName, desc=postDesc)
+    db.session.add(obj)
+
+    db.session.commit() #Commit database changes
+
     print(postName + " " + postDesc)
     return "Post Post Posted"
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
+'''@app.route('/login', methods=['GET', 'POST'])
+def login_view():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # request.form = LoginForm()
+        user = db.session.scalar(sa.select(User).where(User.username == request.form["userName"]))
+        if user is None or not user.check_password(request.form["userPassword"]):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In')'''
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_view():
+    if request.method == 'GET':
+        return render_template('form.html')
+    elif request.method == 'POST':
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        else:
+            user = db.session.scalar(sa.select(User).where(User.username == request.form["userName"])) 
+            if user is None or not user.check_password(request.form["userPassword"]):
+                return redirect(url_for('index'))
+            login_user(user)
+            return redirect(url_for('index'))
+    
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
